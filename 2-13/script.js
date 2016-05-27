122b5302c2d396956643e12a76e70843a4e2ea78
Nikita Kuzmin <backd80@gmail.com>
2016-05-27 13:57:24 +0300

    Add tasts 2-13 and 2-15

---
+++ 2-13/script.js
@@ -0,0 +1,12 @@
+var age = +prompt('How old are you?');
+var msg;
+
+if ((age >= 14) && (age <= 90)) {
+    msg = 'Good';
+    alert(msg);
+}
+
+// if(!(age >= 14 && age <=90)) {}
+// if(age <=14 || age >=90) {}
+
+
